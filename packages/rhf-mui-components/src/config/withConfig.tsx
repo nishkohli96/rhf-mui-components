9d51170776ab4e957e7b07a41a4f1333025e5f70
Nishant <nishkohli96@gmail.com>
2024-08-15 17:12:41 +0530

    update withConfig

--- packages/rhf-mui-components/src/config/withConfig.tsx
+++ packages/rhf-mui-components/src/config/withConfig.tsx
@@ -1,21 +1,23 @@
-import { ComponentType } from 'react';
+import React, { ComponentType, useContext } from 'react';
 import { RHFMuiConfigContext } from './ConfigProvider';
 import { RHFMuiConfig } from '../types';
 
+/**
+ * TODO: somehow this hoc implementation is causing ts
+ * err with register, need to investigate
+ */
+
 const withConfigHOC = <P extends object>(
   WrappedComponent: ComponentType<P>
-): ComponentType<P & RHFMuiConfig> => {
-  const WithConfigComponent = (props: P) => (
-    <RHFMuiConfigContext.Consumer>
-      {(context) => (
-        <WrappedComponent {...props} {...context} />
-      )}
-    </RHFMuiConfigContext.Consumer>
-  );
+): ComponentType<Omit<P, keyof RHFMuiConfig>> => {
+  const WithConfigComponent = (props: Omit<P, keyof RHFMuiConfig>) => {
+    const context = useContext(RHFMuiConfigContext);
+    return <WrappedComponent {...(props as P)} {...context} />;
+  };
 
   const wrappedComponentName =
     WrappedComponent.displayName || WrappedComponent.name || 'Component';
-  WithConfigComponent.displayName = `withTheme(${wrappedComponentName})`;
+  WithConfigComponent.displayName = `withConfig(${wrappedComponentName})`;
 
   return WithConfigComponent;
 };
