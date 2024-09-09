7284bf24c04843d2bbf9547a1cde18a159bd0a7e
nishant.kohli <nishant.kohli@kellton.com>
2024-09-09 13:47:46 +0530

    fix customization page import err

--- apps/rhf-mui-docs/src/page-components/customization/index.tsx
+++ apps/rhf-mui-docs/src/page-components/customization/index.tsx
@@ -1,19 +1,11 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { DefaultRHFMuiConfig } from '@nish1896/rhf-mui-components';
+import TableContent from './TableContent';
 
 export function DefaultConfigValuesTable() {
-	const header = '| Prop Name | Default Value |\n|-|-|\n';
-	
-	const row1 = `| defaultFormLabelSx | ${JSON.stringify(DefaultRHFMuiConfig.defaultFormLabelSx)} |\n`;
-	const row2 = `| defaultFormHelperTextSx | ${JSON.stringify(DefaultRHFMuiConfig.defaultFormHelperTextSx)} |\n `;
-	const row3 = `| dateAdapter | AdapterDayjs |`
-	
-	const tableContent = header + row1 + row2 + row3;
-
 	return (
     <Markdown remarkPlugins={[remarkGfm]}>
-      {tableContent}
+      {TableContent}
     </Markdown>
   );
 }
