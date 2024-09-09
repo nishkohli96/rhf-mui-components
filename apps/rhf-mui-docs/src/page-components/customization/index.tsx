201363b2bb96cd4c544761b098ba872c05df5c47
nishant.kohli <nishant.kohli@kellton.com>
2024-09-09 16:27:13 +0530

    comment customization code

--- apps/rhf-mui-docs/src/page-components/customization/index.tsx
+++ apps/rhf-mui-docs/src/page-components/customization/index.tsx
@@ -3,10 +3,9 @@ import remarkGfm from 'remark-gfm';
 import TableContent from './TableContent';
 
 export default function DefaultConfigValuesTable() {
-  const str = TableContent(); '**hi**';
+  const str = TableContent();
 	return (
     <Markdown remarkPlugins={[remarkGfm]}>
-      {/* {} */}
       {str}
     </Markdown>
     // <p>Helo</p>
