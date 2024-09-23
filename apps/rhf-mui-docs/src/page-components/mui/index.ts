6d53c52714c71e962d0e803c578e75b22f12746c
Nishant <nishkohli96@gmail.com>
2024-09-24 03:27:32 +0530

    mv docs files

---
+++ apps/rhf-mui-docs/src/page-components/mui/index.ts
@@ -0,0 +1,7 @@
+export * from './checkbox';
+export * from './inputs';
+export * from './radio-group';
+export * from './rating';
+export * from './select';
+export * from './slider';
+export * from './switch';
