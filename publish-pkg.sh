f52f48a35198cd106407e2550b9429fe59844952
Nishant <nishkohli96@gmail.com>
2024-09-27 01:57:21 +0530

    build scripts

--- publish-pkg.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "packages/rhf-mui-components/dist" ]; then
-  echo "Publishing package on npm..."
-	cd packages/rhf-mui-components/dist
-	npm profile get
-	npm publish --access public
-else
-  echo "Build does not exist."
-fi
