bad36ec1d72f524c33785ba63d52ec2388f4b86b
Nishant <nishkohli96@gmail.com>
2024-09-27 22:52:52 +0530

    correct csb links

--- apps/rhf-mui-docs/src/page-components/customization/index.tsx
+++ apps/rhf-mui-docs/src/page-components/customization/index.tsx
@@ -6,8 +6,8 @@ export default function DefaultConfigValuesTable() {
   const row1 = '| defaultFormLabelSx | { marginBottom: "0.75rem" } |\n';
   const row2 = '| defaultFormControlLabelSx | {} |\n';
   const row3 = '| defaultFormHelperTextSx | { marginTop: "0.25rem", marginLeft: 0 } |\n';
-  const row4 = '| dateAdapter | AdapterDayjs |\n';
-  const content = header + row1 + row2 + row3 + row4;
+
+  const content = header + row1 + row2 + row3;
   return (
     <Markdown remarkPlugins={[remarkGfm]}>
       {content}
