85b30f888a69b95df500764f4b39df703f2d82ce
Nishant Kohli <nishkohli96@gmail.com>
2024-11-01 22:05:20 +0530

    v1.0.3-docs - Update Website Layout (#9)
    * update docs website
    
    * rm tabs from peerDependencies

--- apps/rhf-mui-docs/src/constants/internal-links.ts
+++
@@ -1,6 +0,0 @@
-const InternalLinks = Object.freeze({
-  demo: 'https://rhf-mui-components-examples.netlify.app/',
-  playground: 'https://codesandbox.io/p/devbox/rhf-mui-components-examples-y8lj9l'
-});
-
-export default InternalLinks;
--- apps/rhf-mui-docs/src/pages/index.tsx
+++ apps/rhf-mui-docs/src/pages/index.tsx
@@ -6,10 +6,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import { HomePageButton } from '@site/src/components';
-import { InternalLinks } from '@site/src/constants';
 import theme from '@site/src/styles/theme';
 import styles from './index.module.css';
 
@@ -17,39 +14,16 @@ const IntroductionBtn = () => (
   <HomePageButton text="Get Started" href="/introduction" bgColor="#25C19F" />
 );
 
-const ExamplesBtn = () => (
-  <HomePageButton
-    text="View Examples"
-    href={InternalLinks.demo}
-    bgColor="#54C7EC"
-  />
-);
-
-const PlaygroundBtn = () => (
-  <HomePageButton
-    text="Playground"
-    href={InternalLinks.playground}
-    bgColor="#ff6b6b"
-  />
-);
-
 const HomepageButtons = () => {
-  const theme = useTheme();
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
-
   return (
     <Grid container justifyContent="center" sx={{ mt: '40px' }}>
       <Box
         sx={{
           display: 'flex',
-          flexDirection: isLargeScreen ? 'row' : 'column',
-          gap: isLargeScreen ? '30px' : '25px',
           alignItems: 'center'
         }}
       >
         <IntroductionBtn />
-        <ExamplesBtn />
-        <PlaygroundBtn />
       </Box>
     </Grid>
   );
