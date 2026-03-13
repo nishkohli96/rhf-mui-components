88aa0b35e567e901af388e78be679428fbde8334
Nishant <nishkohli96@gmail.com>
2026-03-13 23:52:08 +0530

    fix next app build failing

--- apps/rhf-mui-demo/next.config.ts
+++ apps/rhf-mui-demo/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.join(__dirname),
-  },
-  outputFileTracingRoot: __dirname,
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
