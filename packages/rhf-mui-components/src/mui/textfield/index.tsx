6b02bcd66a814ebdbc768399c529a72ea86831c2
Nishant <nishkohli96@gmail.com>
2026-03-16 01:24:34 +0530

    useForward ref in RHFTextfield

--- packages/rhf-mui-components/src/mui/textfield/index.tsx
+++ packages/rhf-mui-components/src/mui/textfield/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useContext, type ReactNode, type ChangeEvent } from 'react';
-import get from 'lodash/get';
+import { useContext, forwardRef, type ReactNode, type ChangeEvent, type Ref, type JSX } from 'react';
 import {
   Controller,
+  get,
   useFormState,
   type FieldValues,
   type Path,
@@ -20,7 +20,7 @@ import {
   defaultAutocompleteValue
 } from '@/common';
 import type { FormLabelProps, FormHelperTextProps, TextFieldProps } from '@/types';
-import { fieldNameToLabel, keepLabelAboveFormField } from '@/utils';
+import { fieldNameToLabel, keepLabelAboveFormField, mergeRefs } from '@/utils';
 
 export type RHFTextFieldProps<T extends FieldValues> = {
   fieldName: Path<T>;
@@ -43,26 +43,30 @@ export type RHFTextFieldProps<T extends FieldValues> = {
   formHelperTextProps?: FormHelperTextProps;
 } & TextFieldProps;
 
-const RHFTextField = <T extends FieldValues>({
-  fieldName,
-  control,
-  registerOptions,
-  customOnChange,
-  onValueChange,
-  label,
-  showLabelAboveFormField,
-  hideLabel,
-  formLabelProps,
-  required,
-  helperText,
-  errorMessage,
-  hideErrorMessage,
-  formHelperTextProps,
-  onBlur,
-  autoComplete = defaultAutocompleteValue,
-  ref,
-  ...rest
-}: RHFTextFieldProps<T>) => {
+const RHFTextField = forwardRef(function RHFTextField<
+  T extends FieldValues
+>(
+  {
+    fieldName,
+    control,
+    registerOptions,
+    customOnChange,
+    onValueChange,
+    label,
+    showLabelAboveFormField,
+    hideLabel,
+    formLabelProps,
+    required,
+    helperText,
+    errorMessage,
+    hideErrorMessage,
+    formHelperTextProps,
+    onBlur,
+    autoComplete = defaultAutocompleteValue,
+    ...rest
+  }: RHFTextFieldProps<T>,
+  ref: Ref<HTMLInputElement>
+) {
   const { allLabelsAboveFields } = useContext(RHFMuiConfigContext);
   const fieldLabel = label ?? fieldNameToLabel(fieldName);
   const isLabelAboveFormField = keepLabelAboveFormField(
@@ -124,7 +128,7 @@ const RHFTextField = <T extends FieldValues>({
                 onBlur?.(blurEvent);
               }}
               error={isError}
-              inputRef={rhfRef}
+              inputRef={mergeRefs(rhfRef, ref)}
               {...rest}
               {...otherFieldParams}
             />
@@ -140,6 +144,8 @@ const RHFTextField = <T extends FieldValues>({
       />
     </FormControl>
   );
-};
+}) as <T extends FieldValues>(
+  props: RHFTextFieldProps<T> & { ref?: Ref<HTMLInputElement> }
+) => JSX.Element;
 
 export default RHFTextField;
--- packages/rhf-mui-components/src/utils/control.ts
+++ packages/rhf-mui-components/src/utils/control.ts
@@ -1,6 +1,23 @@
+import { type Ref } from 'react';
+
 export function keepLabelAboveFormField(
   showLabelAboveFormField?: boolean,
   allLabelsAboveFields?: boolean
 ) {
   return Boolean(showLabelAboveFormField ?? allLabelsAboveFields);
+}
+
+export function mergeRefs<T>(...refs: (Ref<T> | undefined)[]) {
+  return (value: T | null) => {
+    refs.forEach(ref => {
+      if (!ref) {
+        return;
+      }
+      if (typeof ref === 'function') {
+        ref(value);
+      } else {
+        (ref).current = value;
+      }
+    });
+  };
 }
