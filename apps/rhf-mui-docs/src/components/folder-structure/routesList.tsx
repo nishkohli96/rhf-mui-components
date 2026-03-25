780d2e3e0550ae18d14d23d4ce31f529cbe886d6
Nishant Kohli <nishkohli96@gmail.com>
2026-03-26 00:07:13 +0530

    update docs with newly added components

--- apps/rhf-mui-docs/src/components/folder-structure/routesList.tsx
+++ apps/rhf-mui-docs/src/components/folder-structure/routesList.tsx
@@ -1,47 +1,57 @@
+import { MuiComponents, MuiPickersComponents, MiscComponents } from '@site/src/types/component-names';
+
 const rootDir = '/components';
 const muiPrefix = '/mui';
 const muiPickersPrefix = '/mui-pickers';
 const miscPrefix = '/misc';
 
 const muiComponents = [
-  'RHFTextField',
-  'RHFNumberInput',
-  'RHFPasswordInput',
-  'RHFTagsInput',
-  'RHFFileUploader',
-  'RHFSelect',
-  'RHFNativeSelect',
-  'RHFAutocomplete',
-  'RHFMultiAutocomplete',
-  'RHFCountrySelect',
-  'RHFCheckbox',
-  'RHFCheckboxGroup',
-  'RHFRadioGroup',
-  'RHFSlider',
-  'RHFSwitch',
-  'RHFRating'
+  MuiComponents.TextField,
+  MuiComponents.NumberInput,
+  MuiComponents.PasswordInput,
+  MuiComponents.TagsInput,
+  MuiComponents.FileUploader,
+  MuiComponents.Select,
+  MuiComponents.NativeSelect,
+  MuiComponents.Autocomplete,
+  MuiComponents.AutocompleteObject,
+  MuiComponents.MultiAutocomplete,
+  MuiComponents.MultiAutocompleteObject,
+  MuiComponents.CountrySelect,
+  MuiComponents.Checkbox,
+  MuiComponents.CheckboxGroup,
+  MuiComponents.RadioGroup,
+  MuiComponents.Slider,
+  MuiComponents.Switch,
+  MuiComponents.Rating
 ];
 
 const muiPickersComponents = [
-  'RHFDatePicker',
-  'RHFTimePicker',
-  'RHFDateTimePicker'
+  MuiPickersComponents.DatePicker,
+  MuiPickersComponents.TimePicker,
+  MuiPickersComponents.DateTimePicker
 ];
 
 const miscComponents = [
-  'RHFColorPicker',
-  'RHFRichTextEditor',
-  'RHFPhoneInput'
+  MiscComponents.ColorPicker,
+  MiscComponents.RichTextEditor,
+  MiscComponents.PhoneInput
 ];
 
+/** Components added in v2 and before v3.3 */
 export const newlyAddedComponents = [
-  'RHFAutocomplete',
-  'RHFMultiAutocomplete',
-  'RHFCountrySelect',
-  'RHFTagsInput',
-  'RHFPhoneInput',
-  'RHFNumberInput',
-  'RHFFileUploader',
+  MuiComponents.Autocomplete,
+  MuiComponents.MultiAutocomplete,
+  MuiComponents.CountrySelect,
+  MuiComponents.TagsInput,
+  MiscComponents.PhoneInput,
+  MuiComponents.NumberInput,
+  MuiComponents.FileUploader,
+];
+
+export const newlyAddedV3_3Components = [
+  MuiComponents.AutocompleteObject,
+  MuiComponents.MultiAutocompleteObject,
 ];
 
 export const muiFoldersList = muiComponents.map(component => ({
