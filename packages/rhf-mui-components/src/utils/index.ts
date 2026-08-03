e979934a458e101c26eba67b17a3b3dd556a8e97
Nishant <nishkohli96@gmail.com>
2026-08-04 02:19:23 +0530

    rm unused or exports already from rh-mui-components

--- packages/rhf-mui-components/src/utils/index.ts
+++ packages/rhf-mui-components/src/utils/index.ts
@@ -1,7 +1,5 @@
-export * from './array';
 export * from './control';
 export * from './errors';
 export * from './mui';
-export * from './object';
 export * from './text-transform';
 export * from './useFieldIds';
